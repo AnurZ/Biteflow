4b30a5f002d80a8337c8ae8c9b844ae9934d34b0
AnurZ <anuranci@gmail.com>
2025-10-20 20:43:13 +0200

    Added rs1 template files to project

---
+++ Backend/Market.Infrastructure/Database/DatabaseConfiguration.cs
@@ -0,0 +1,74 @@
+﻿using Market.Domain.Common;
+using Market.Infrastructure.Database.Seeders;
+using System.Linq.Expressions;
+
+namespace Market.Infrastructure.Database;
+
+public partial class DatabaseContext
+{
+    private void ModifyTimestamps()
+    {
+        var entries = ChangeTracker.Entries();
+
+        foreach (var entry in entries)
+        {
+            var entity = ((BaseEntity)entry.Entity);
+
+            if (entry.State == EntityState.Added)
+            {
+                entity.CreatedAt = DateTime.Now;
+            }
+            else if (entry.State == EntityState.Modified)
+            {
+                entity.ModifiedAt = DateTime.Now;
+            }
+        }
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<decimal>().HavePrecision(18, 2);
+        configurationBuilder.Properties<decimal?>().HavePrecision(18, 2);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        ApplyGlobalFielters(modelBuilder);
+
+        StaticDataSeeder.Seed(modelBuilder); // static data
+    }
+
+    private void ApplyGlobalFielters(ModelBuilder modelBuilder)
+    {
+        // Apply a global filter to all entities inheriting from BaseEntity
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            if (typeof(BaseEntity).IsAssignableFrom(entityType.ClrType))
+            {
+                var parameter = Expression.Parameter(entityType.ClrType, "e");
+                var prop = Expression.Property(parameter, nameof(BaseEntity.IsDeleted));
+                var compare = Expression.Equal(prop, Expression.Constant(false));
+                var lambda = Expression.Lambda(compare, parameter);
+
+                modelBuilder.Entity(entityType.ClrType)
+                            .HasQueryFilter(lambda);
+            }
+        }
+    }
+
+    public override int SaveChanges()
+    {
+        ModifyTimestamps();
+
+        return base.SaveChanges();
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        ModifyTimestamps();
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ Backend/Market.Shared/Dtos/ErrorDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Market.Shared.Dtos;
+
+public sealed class ErrorDto
+{
+    public string Code { get; init; }     // e.g. "internal.error"
+    public string Message { get; init; }  // short message
+}
