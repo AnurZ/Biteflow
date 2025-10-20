4ca05b926a69ed2f95906563314d5ab4c55008f2
AnurZ <anuranci@gmail.com>
2025-10-20 20:43:13 +0200

    Added rs1 template files to project

---
+++ Backend/Market.API/Program.cs
@@ -0,0 +1,39 @@
+﻿using Market.API;
+using Market.Application;
+using Market.Infrastructure;
+
+public partial class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Registrations by layers
+        builder.Services
+            .AddAPI(builder.Configuration, builder.Environment)
+            .AddInfrastructure(builder.Configuration, builder.Environment)
+            .AddApplication();
+
+        var app = builder.Build();
+
+        // Middleware pipeline
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseMiddleware<ExceptionMiddleware>();
+
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        // Migracije + seeding (centralized in Infrastructure)
+        await app.Services.InitializeDatabaseAsync(app.Environment);
+
+        app.Run();
+    }
+}
