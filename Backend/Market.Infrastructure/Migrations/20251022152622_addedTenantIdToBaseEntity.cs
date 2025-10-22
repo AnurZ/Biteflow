f726e40b72815300c65e495987878cf0018f6422
AnurZ <anuranci@gmail.com>
2025-10-22 17:37:29 +0200

    Added FE Base Project and Updated BE Template

---
+++ Backend/Market.Infrastructure/Migrations/20251022152622_addedTenantIdToBaseEntity.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Market.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedTenantIdToBaseEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "TenantId",
+                table: "Users",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "TenantId",
+                table: "RefreshTokens",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "TenantId",
+                table: "Products",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "TenantId",
+                table: "ProductCategories",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "ProductCategories");
+        }
+    }
+}
