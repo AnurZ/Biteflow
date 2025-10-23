f40db9baa0541188b5d41aeba72acf01ee543de3
AnurZ <anuranci@gmail.com>
2025-10-23 14:19:43 +0200

    FE base project refactor

--- Frontend/src/app/modules/core/auth/auth-module.ts
+++ Frontend/src/app/modules/core/auth/auth-module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Login } from './login/login';
+import {AuthRoutingModule} from './auth-routing.module';
 
 
 
@@ -9,7 +10,8 @@ import { Login } from './login/login';
     Login
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule,
   ]
 })
 export class AuthModule { }
