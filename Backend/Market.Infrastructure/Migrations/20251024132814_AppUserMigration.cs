329f08161de2be54fd9c1040e6a3fe7fc4a92b9b
AnurZ <anuranci@gmail.com>
2025-10-24 15:42:01 +0200

    Task/A3-Update-AppUser-and-JWT-Claims-to-reflect-DB-Schema

---
+++ Backend/Market.Infrastructure/Migrations/20251024132814_AppUserMigration.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Market.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AppUserMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "IsManager",
+                table: "Users",
+                newName: "IsLocked");
+
+            migrationBuilder.RenameColumn(
+                name: "IsEmployee",
+                table: "Users",
+                newName: "IsEmailConfirmed");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EncryptedSensitiveData",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RestaurantId",
+                table: "Users",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "EncryptedSensitiveData",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RestaurantId",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "IsLocked",
+                table: "Users",
+                newName: "IsManager");
+
+            migrationBuilder.RenameColumn(
+                name: "IsEmailConfirmed",
+                table: "Users",
+                newName: "IsEmployee");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
