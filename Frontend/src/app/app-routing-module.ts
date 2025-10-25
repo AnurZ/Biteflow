908a5526f20958f7e50b8a7f84d02ef6696736e7
AnurZ <anuranci@gmail.com>
2025-10-25 11:39:17 +0200

    task/A5-Add-basic-shared-navbar-component-to-encapsulate-login-and-logout

--- Frontend/src/app/app-routing-module.ts
+++ Frontend/src/app/app-routing-module.ts
@@ -6,6 +6,10 @@ const routes: Routes = [
     path: 'auth',
     loadChildren: () => import('./modules/core/auth/auth-module').then(m => m.AuthModule)  // Lazy loaded module
   },
+  {
+    path: 'public',
+    loadChildren: () => import('./modules/public/public-module').then(m => m.PublicModule)
+  },
   {path: '**', redirectTo: 'public', pathMatch: 'full'}  // Default route
 ];
 
