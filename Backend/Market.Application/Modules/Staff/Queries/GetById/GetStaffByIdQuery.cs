009a343d8e5ae9f04efc6ec3e4d48e61e2e83a8f
AnurZ <anuranci@gmail.com>
2025-10-26 12:14:27 +0100

    task/A8-Define-StaffController-Commands-CommandHandlers-and-DTOs

---
+++ Backend/Market.Application/Modules/Staff/Queries/GetById/GetStaffByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Market.Application.Modules.Staff.Queries.GetById
+{
+    public sealed class GetStaffByIdQuery : IRequest<GetStaffByIdDto>
+    {
+        public int Id { get; init; }
+    }
+}
