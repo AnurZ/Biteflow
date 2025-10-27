49b4b9d0739ca3807f8c9f45173584e712ee71b2
AnurZ <anuranci@gmail.com>
2025-10-27 11:08:05 +0100

    task/A10-Implement-FE-Staff-Component-Admin-Routing-lazy-loading-and-define-dtos

---
+++ Frontend/src/app/modules/admin/staff/staff-form-dialog/staff-form-dialog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-staff-form-dialog',
+  standalone: false,
+  templateUrl: './staff-form-dialog.html',
+  styleUrl: './staff-form-dialog.css'
+})
+export class StaffFormDialog {
+
+}
