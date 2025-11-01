811f630a3339cf5e6826a03e9a6f32f3f4f50013
AnurZ <anuranci@gmail.com>
2025-11-01 13:47:46 +0100

    task/A14-Define-TenantActivationController-Commands-CommandHandlers-Dtos

---
+++ Backend/Market.Application/Modules/TenantActivation/Commands/ApproveRequest/ApproveRequestCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Application.Modules.TenantActivation.Commands.ApproveRequest
+{
+    public sealed class ApproveRequestCommandHandler(IAppDbContext db, IActivationLinkService links)
+    : IRequestHandler<ApproveRequestCommand, string>
+    {
+        public async Task<string> Handle(ApproveRequestCommand r, CancellationToken ct)
+        {
+            var e = await db.TenantActivationRequests.FindAsync([r.Id], ct)
+                    ?? throw new MarketNotFoundException("Request not found");
+            var link = await links.IssueLinkAsync(e.Id, ct);
+            e.Approve(link);
+            await db.SaveChangesAsync(ct);
+            return link;
+        }
+    }
+
+}
