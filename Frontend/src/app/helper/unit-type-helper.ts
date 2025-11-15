8b04bb7a8ea4b7a62c97f24fba7de60ee085bb46
Menso <mensur.cavak@edu.fit.ba>
2025-11-15 14:09:18 +0100

    local changes before merge

---
+++ Frontend/src/app/helper/unit-type-helper.ts
@@ -0,0 +1,14 @@
+﻿import { UnitTypes} from '../modules/meals/meals-model';
+
+// Convert enum number to readable string (for UI)
+export function mapUnitTypeToString(unitType: string | number | undefined): string {
+  if (unitType === undefined || unitType === null) return UnitTypes[UnitTypes.Unit]; // default: "Unit"
+  return typeof unitType === 'number' ? UnitTypes[unitType] : unitType;
+}
+
+// Convert string (UI) to enum number (for API)
+export function mapUnitTypeToNumber(unitType: string | number): number {
+  return typeof unitType === 'string'
+    ? UnitTypes[unitType as keyof typeof UnitTypes]
+    : unitType;
+}
