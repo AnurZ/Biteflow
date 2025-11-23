1a75b9260a09dfdf402792a33096fc434a571096
Menso <mensur.cavak@edu.fit.ba>
2025-11-23 20:48:30 +0100

    task/M14-Backend-Added-TableReservation-entity-configuration-file-create-command-and-get-list-query-with-filters

---
+++ Backend/Market.Domain/Common/Enums/ReservationStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Domain.Common.Enums
+{
+    public enum ReservationStatus
+    {
+        Pending = 0,     // Before the guest arrives
+        Confirmed = 1,   // Staff confirmed it
+        Completed = 2,   // Guest came and finished
+        Cancelled = 3,
+        NoShow = 4
+    }
+
+}
---
+++ Backend/Market.Domain/Common/Enums/TableStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Domain.Common.Enums
+{
+    public enum TableStatus
+    {
+        Free = 0,
+        Occupied = 1,
+        Reserved = 2,
+        Cleaning = 3,
+        OutOfService = 4
+    }
+
+}
