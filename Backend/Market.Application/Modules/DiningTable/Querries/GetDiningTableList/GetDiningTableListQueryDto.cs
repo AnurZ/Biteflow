b24e1f6ca4252639c37e38f0fc7fe2dafde45b23
Menso <mensur.cavak@edu.fit.ba>
2025-11-23 20:48:30 +0100

    task/M14-Backend-Added-TableReservation-entity-configuration-file-create-command-and-get-list-query-with-filters

---
+++ Backend/Market.Application/Modules/DiningTable/Querries/GetDiningTableList/GetDiningTableListQueryDto.cs
@@ -0,0 +1,27 @@
+﻿using Market.Application.Modules.MealCategory.Querries.GetMealCategories;
+using Market.Domain.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Application.Modules.DiningTable.Querries.GetDiningTableList
+{
+    public sealed class GetDiningTableListQuery : IRequest<List<GetDiningTableListQueryDto>>
+    {
+        public string? SectionName { get; set; }
+        public TableStatus? Status { get; set; }
+        public int? MinimumSeats { get; set; }
+    }
+    public sealed class GetDiningTableListQueryDto
+    {
+        public int Id { get; set; }
+        public string SectionName { get; set; }
+        public int Number { get; set; }
+        public int NumberOfSeats { get; set; }
+        public TableTypes TableType { get; set; }
+        public TableStatus Status { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Backend/Market.Infrastructure/Database/Configurations/DiningTable/DiningTableConfiguration.cs
@@ -0,0 +1,56 @@
+﻿using Market.Domain.Common.Enums;
+using Market.Domain.Entities.DiningTables;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Market.Infrastructure.Persistence.Configurations
+{
+    public class DiningTableConfiguration : IEntityTypeConfiguration<DiningTable>
+    {
+        public void Configure(EntityTypeBuilder<DiningTable> builder)
+        {
+            // Table name
+            builder.ToTable("DiningTables");
+
+            // Key
+            builder.HasKey(t => t.Id);
+
+            // Properties
+            builder.Property(t => t.SectionName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(t => t.Number)
+                .IsRequired();
+
+            builder.Property(t => t.NumberOfSeats)
+                .IsRequired();
+
+            builder.Property(t => t.IsActive)
+                .HasDefaultValue(true);
+
+            builder.Property(t => t.TableType)
+                .IsRequired();
+
+            builder.Property(t => t.Status)
+                .HasDefaultValue(TableStatus.Free)
+                .IsRequired();
+
+            builder.Property(t => t.LastUsedAt)
+                .IsRequired(false);
+
+            builder.HasMany(t => t.Reservations)
+                .WithOne(r => r.DiningTable)
+                .HasForeignKey(r => r.DiningTableId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasMany(t => t.Orders)
+                .WithOne(o => o.DiningTable)
+                .HasForeignKey(o => o.DiningTableId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasIndex(t => new { t.SectionName, t.Number })
+                .IsUnique();
+        }
+    }
+}
