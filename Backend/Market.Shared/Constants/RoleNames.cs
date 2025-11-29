6a5073ee7b32b00b9e415928925e5da6f7b4f9b5
AnurZ <anuranci@gmail.com>
2025-11-29 14:08:52 +0100

    A28---Add-a-customer-role-and-update-seeder

--- Backend/Market.Shared/Constants/RoleNames.cs
+++ Backend/Market.Shared/Constants/RoleNames.cs
@@ -3,4 +3,5 @@ public static class RoleNames
     public const string SuperAdmin = "superadmin";
     public const string Admin = "admin";
     public const string Staff = "staff";
+    public const string Customer = "customer";
 }
