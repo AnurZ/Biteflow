3d83996c9a8e979eaef5cd66035d2121a6da4ca7
Menso <mensur.cavak@edu.fit.ba>
2026-05-15 19:29:07 +0200

    Backend data import implementation

---
+++ Backend/Market.Application/Modules/DataImport/OrderImport/OrderImportRowDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Application.Modules.DataImport.OrderImport
+{
+    public sealed class OrderImportRowDto
+    {
+        public int? OrderId { get; set; }
+
+        public string Status { get; set; }
+
+        public int? DiningTableId { get; set; }
+
+        public int? TableNumber { get; set; }
+
+        public string? Notes { get; set; }
+    }
+}
